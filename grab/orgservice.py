e2e83e06a3318407ef7d3412995c1098970df222
louis <yia0223@163.com>
2018-07-25 18:45:16 +0800

    re

--- grab/orgservice.py
+++ grab/orgservice.py
@@ -4,6 +4,7 @@ import urllib.parse
 from bs4 import BeautifulSoup
 from fontTools.ttLib import TTFont
 import threading
+from random import choice
 
 loop = asyncio.get_event_loop()
 #asyncio.Semaphore(),限制同时运行协程数量
@@ -21,19 +22,21 @@ class OrgServiceI:
 		self._referer = "https://%s" % (self._host)
 		#TODO: make it automatic
 		#auth_token = "eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiIxODkwMjI5NTYxNiIsImlhdCI6MTUzMjA3NjI0MiwiZXhwIjoxNTQ3NjI4MjQyfQ.ZTPE7eq0xMsPn5KStuEkI9FuqLYgianxWT9fY8pyHfyGPUaoKkIzaVgwg1TXrkJhwjMCHDIt765jgpxxON1GGQ"
-		self._cookie = 'auth_token=eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiIxODkwMjI5NTYxNiIsImlhdCI6MTUzMjA3NjI0MiwiZXhwIjoxNTQ3NjI4MjQyfQ.ZTPE7eq0xMsPn5KStuEkI9FuqLYgianxWT9fY8pyHfyGPUaoKkIzaVgwg1TXrkJhwjMCHDIt765jgpxxON1GGQ'
+		self._cookie = '_utm=789100e01f4f43ecac46283cb22af246;undefined=912189508fef11e8b2be59964ed454f3;token=4332c35ecc954d269e285f59799f8d46;TYCID=912189508fef11e8b2be59964ed454f3;csrfToken=KuYG7GOrQccKznCmP4R2ErYy;auth_token=eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiIxODkwMjI5NTYxNiIsImlhdCI6MTUzMjUxMjAyNCwiZXhwIjoxNTQ4MDY0MDI0fQ.DJzjiNcS97IYnNH5-bmjxsywQv1qrLDK_AZ4VDEvNC-ZKBZ1ospzBPLbUTvo06dn2ntHcGXiPNK4BbfozQZEVg;ssuid=8561655591;tyc-user-info=%257B%2522token%2522%253A%2522eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiIxODkwMjI5NTYxNiIsImlhdCI6MTUzMjUxMjAyNCwiZXhwIjoxNTQ4MDY0MDI0fQ.DJzjiNcS97IYnNH5-bmjxsywQv1qrLDK_AZ4VDEvNC-ZKBZ1ospzBPLbUTvo06dn2ntHcGXiPNK4BbfozQZEVg%2522%252C%2522integrity%2522%253A%25220%2525%2522%252C%2522state%2522%253A%25220%2522%252C%2522redPoint%2522%253A%25220%2522%252C%2522vipManager%2522%253A%25220%2522%252C%2522vnum%2522%253A%25220%2522%252C%2522onum%2522%253A%25220%2522%252C%2522mobile%2522%253A%252218902295616%2522%257D'
 		self._fontCachedPath = "./fonts/%s.woff"
 		#_session = "lnpi16dbjvd83ftb43atc8lg77"
 		#_zgDid = "%7B%22did%22%3A%20%22164a6dcfcc71291-09ff3ca9947395-5b193413-1fa400-164a6dcfcc879%22%7D"
 		#self._cookie = "PHPSESSID=%s;zg_did=%s;" % (_session, _zgDid)
 		#get PHPSESSID
-		self._useragent = "Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/67.0.3396.99 Safari/537.36"
+		self._useragent = "Mozilla/5.0 (Windows NT 6.1; Win64; x64; rv:62.0) Gecko/20100101 Firefox/62.0"
 		self._fontUrl = "https://static.tianyancha.com/fonts-styles/fonts/%s/%s/tyc-num.woff"
 		self._headers = {
-			"Accept" : "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8",
+			"Accept" : "text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8",
 			"Accept-Encoding" : "gzip, deflate, br",
-			#"Host" : self._host,
-			#"Connection" : "keep-alive",
+			"Accept-Language" : "en-US,en;q=0.5",
+			"Cache-Control" : "max-age=0",
+			"Connection" : "keep-alive",
+			"Host" : self._host,
 			"Referer" : self._referer,
 			"Cookie" : self._cookie,
 			"User-Agent" : self._useragent
@@ -45,27 +48,16 @@ class OrgServiceI:
 			"公司状态" : "N/A",
 			"法人" : "N/A"
 		}
-
 		self._re = re.compile(r'/company/[^/]+')
+		self._proxy = "http://101.236.60.52:8866"
 
-		self._secret = loop.run_until_complete(self.downloadFont())
-		# event_loop.close()
-		#1985-09-15
-		#8076-40-89
-		#3990813.182
-		#5004785.872
-
-		#2018-0 -12
-		#2487-41-82	
-	# def resetCookie(self):
-	# 	self._headers["Cookie"] = self._headers["Cookie"] + str(int(time.time()))
 
 	async def getQuery(self, name=None, url=None):
 		# self.resetCookie()
 		async with sem:
 			async with aiohttp.ClientSession() as session:
 				if name:
-					async with session.get(self._url + urllib.parse.quote(name),  headers=self._headers) as resp:
+					async with session.get(self._url + urllib.parse.quote(name),  headers=self._headers, proxy=self._proxy) as resp:
 						assert resp.status == 200
 						html = await resp.text()
 						soup = BeautifulSoup(html, 'lxml')
@@ -73,7 +65,7 @@ class OrgServiceI:
 						aTag = soup.find("a", text=name)
 						if aTag:
 							#self.resetCookie()
-							async with session.get(aTag["href"],  headers=self._headers) as _resp:
+							async with session.get(aTag["href"],  headers=self._headers, proxy=self._proxy) as _resp:
 								assert _resp.status == 200
 								return await _resp.text()
 						else:
@@ -85,59 +77,54 @@ class OrgServiceI:
 						return await _resp.text()
 		
 
-	async def downloadFont(self):
+	async def shouldParseFont(self, bodyClass):
 		# self.resetCookie()
-		async with aiohttp.ClientSession() as session:
-			async with session.get("https://" + self._host,  headers=self._headers) as resp:
-				assert resp.status == 200
-				html = await resp.text()
-				soup = BeautifulSoup(html, 'lxml')
-				[s.extract() for s in soup.find_all("img")]
-				bodyClass = soup.body["class"][0].replace("font-", "")
-				print(bodyClass)
-				file = self._fontCachedPath % (bodyClass)
-				if not os.path.exists(file):
-					#self.resetCookie()
-					async with session.get(self._fontUrl % (bodyClass[0:2], bodyClass),  headers=self._headers) as _resp:
-						assert _resp.status == 200
-						with open(file, 'wb') as fd:
-							while True:
-								chunk = await _resp.content.read(1024)
-								if not chunk:
-									break
-								fd.write(chunk)
-						font = TTFont(file)
-						#font.saveXML("./fonts/1.xml")
-						gly_list = font.getGlyphOrder()
-						gly_names = font.getGlyphNames()
-						gly_list = gly_list[2:12]
-						gly_names = gly_names[0:10]
-						secrets = {}
-						for i in range(10):
-							secrets[gly_list[i]] = gly_names[i]
-						# for number, gly in enumerate(gly_list):
-						# 	# print(str(number) + ":" + gly)
-						# 	if re.match(r'\d+', gly):
-						# 		secrets[gly] = str(number)
-						self._redis.set(bodyClass, json.dumps(secrets))
-						return secrets
-				else:
-					return json.loads(self._redis.get(bodyClass))
+		print(bodyClass)
+		file = self._fontCachedPath % (bodyClass)
+		if not os.path.exists(file):
+			#self.resetCookie()
+			async with aiohttp.ClientSession() as session:
+				async with session.get(self._fontUrl % (bodyClass[0:2], bodyClass),  headers=self._headers) as _resp:
+					assert _resp.status == 200
+					with open(file, 'wb') as fd:
+						while True:
+							chunk = await _resp.content.read(1024)
+							if not chunk:
+								break
+							fd.write(chunk)
+					font = TTFont(file)
+					#font.saveXML("./fonts/1.xml")
+					gly_list = font.getGlyphOrder()
+					gly_names = font.getGlyphNames()
+					gly_list = gly_list[2:12]
+					gly_names = gly_names[0:10]
+					secrets = {}
+					for i in range(10):
+						secrets[gly_list[i]] = gly_names[i]
+
+					self._redis.set(bodyClass, json.dumps(secrets))
+					return secrets
+		else:
+			return json.loads(self._redis.get(bodyClass))
 
 	def sub(self, str):
 		return re.sub(r'\|[^\|]+', '', str)
 
-	def convert(self, num):
+	def convert(self, secrets, num):
 		num = re.sub(r'[^\d\.-]+', '', num)
 		str = ''
 		for n in list(num):
-			str += self._secret[n] if n in self._secret else n
+			str += secrets[n] if n in secrets else n
 		return str
 
-	def parse(self, html):
+	async def parse(self, html):
 		soup = BeautifulSoup(html, 'lxml')
 		[s.extract() for s in soup.find_all("img")]
 		#print(soup.find("div", class_="boss-td").get_text("|", strip=True))
+		bodyClass = soup.body["class"][0].replace("font-", "")
+
+		secrets = await self.shouldParseFont(bodyClass)
+
 		faren = soup.find("div", class_="humancompany")
 		if faren:
 		#print(faren.get_text("|", strip=True))
@@ -150,11 +137,11 @@ class OrgServiceI:
 			tr = td.find_parent("tr")
 		capital = td.find_next_sibling().find("text", class_="tyc-num lh24")
 		if capital:
-			self._fields["注册资本"] = self.convert(capital.get_text(strip=True))
+			self._fields["注册资本"] = self.convert(secrets, capital.get_text(strip=True))
 		tr = tr.find_next_sibling()
 		founded = tr.find("text", class_="tyc-num lh24")
 		if founded:
-			self._fields["注册时间"] = self.convert(founded.get_text(strip=True))
+			self._fields["注册时间"] = self.convert(secrets, founded.get_text(strip=True))
 		tr = tr.find_next_sibling()
 		self._fields["公司状态"] = tr.find("div", class_="num-opening").get_text(strip=True)
 		#
@@ -164,7 +151,7 @@ class OrgServiceI:
 		for index, row in enumerate(table.find_all("td"), start=0):
 			if index % 2 == 0:
 				if row.find_next_sibling().find("text", class_="tyc-num lh24"):
-					self._fields[self.sub(row.get_text("|", strip=True))] = self.convert(row.find_next_sibling().get_text(strip=True))
+					self._fields[self.sub(row.get_text("|", strip=True))] = self.convert(secrets, row.find_next_sibling().get_text(strip=True))
 				else:
 					self._fields[self.sub(row.get_text("|", strip=True))] = self.sub(row.find_next_sibling().get_text("|", strip=True))
 		
@@ -193,6 +180,10 @@ class OrgServiceI:
 			t = threading.Thread(target=self.analyse, args=(url,))
 			t.start()
 
+		#变更记录
+		ci = soup.find("div", id="_container_changeinfo")
+
+
 	def analyse(self, urls):
 		_loop = asyncio.new_event_loop()
 		asyncio.set_event_loop(_loop)
@@ -202,12 +193,10 @@ class OrgServiceI:
 	async def parseOrgInfo(self, name=None, url=None):
 		result = await self.getQuery(name=name, url=url)
 		if result:
-			self.parse(result)
+			await self.parse(result)
 
 	def getOrgInfo(self, name=None, url=None):
 		loop.run_until_complete(self.parseOrgInfo(name=name, url=url))
-		#print(urls)
-		#loop.run_until_complete(asyncio.gather(*[self.parseOrgInfo(url=u) for u in urls]))
 
 				
 if __name__ == '__main__':
--- grab/proxy.py
+++ grab/proxy.py
@@ -1,43 +1,93 @@
-import os, asyncio, aiohttp, json, time
+import os, asyncio, aiohttp, json, time, redis
+import sched
+from bs4 import BeautifulSoup
 #www.tianyancha.com
-#http://www.xiongmaodaili.com/freeproxy
+#http://www.xicidaili.com/nn/
+
+schedule = sched.scheduler(time.time, time.sleep) 
+loop = asyncio.get_event_loop()
+sem = asyncio.Semaphore(10)
 
 class Proxy:
 
+	def __init__(self, ip, score):
+		self.ip = ip
+		self.score = score
+
+	def __lt__(self, other):
+		return self.score > other.score
+
+	def __str__(self):
+		return json.dumps(self.ip)
+
+class ProxyEncoder(json.JSONEncoder):
+	def default(self, o):
+		if isinstance(o, Proxy):
+			return o.__str__()
+		return json.JSONEncoder.default(o)
+
+class ProxyService:
+
 	def __init__(self):
 
-		self._proxyUrl = "http://www.xiongmaodaili.com/xiongmao-web/freeip/list"
+		pool = redis.ConnectionPool(host="192.168.10.208", port=45000, decode_responses=True)
+		self._redis = redis.Redis(connection_pool=pool)
 
-		self._validateUrl = "https://www.tianyancha.com"
+		self._proxyUrls = "http://www.xicidaili.com/nn/", 
 
-		self.conn = aiohttp.TCPConnector(ssl=False, limit=100, use_dns_cache=True)
+		self._validateUrl = "https://www.baidu.com/"
 
 		self._headers = {
 			"User-Agent" : "Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/67.0.3396.99 Safari/537.36"
 		}
 
 	async def allips(self):
-		async with aiohttp.ClientSession(connector=self.conn) as session:
+		conn = aiohttp.TCPConnector(ssl=False, limit=200, use_dns_cache=True)
+		async with aiohttp.ClientSession(connector=conn) as session:
 			async with session.get(self._proxyUrl,  headers=self._headers) as resp:
 				assert resp.status == 200
 				rs = json.loads(await resp.text())
 				#print(html)
 				arr = rs["obj"]
 				arr.sort(key = lambda x:x["responseTime"])
+				print(arr)
 				return arr;
+
+	async def tyc(self, ip, session):
+		_proxy = 'http://' + ip["ip"] + ":" + str(ip["port"])
+		#print(_proxy)
+		try:
+			start = time.time()
+			async with session.get(self._validateUrl,  headers=self._headers, proxy=_proxy, timeout=5) as resp:
+				assert resp.status == 200
+				await resp.text()
+				end = time.time()
+				print('proxy: %s' % _proxy, 'Used time-->', end - start, 's')
+				return Proxy(ip, end - start)
+		except Exception as e:
+			return Proxy(ip, 99999)
+			
 
 	async def elect(self):
 		ips = await self.allips()
-		async with aiohttp.ClientSession(connector=self.conn) as session:
-			 for ip in ips:
-			 	_proxy = 'http://' + ip["ip"] + ":" + str(ip["port"])
-			 	start = time.time()
-			 	async with session.get(self._validateUrl,  headers=self._headers, proxy=_proxy, timeout=5) as resp:
-			 		assert resp.status == 200
+		async with aiohttp.ClientSession() as session:
+			return await asyncio.gather(*[self.tyc(ip, session) for ip in ips])
+
+	def run(self):		
+		proxies = loop.run_until_complete(self.elect())
+		proxies = list(filter(lambda x: x.score < 0.5, proxies))
+		proxies.sort(reverse=True)
+		p = proxies[0]
+		self._redis.set("thanos_proxy", "http://" + p.ip["ip"] + ":" + str(p.ip["port"]))
 
 
-
-
+def run_proxy(p, inc):
+	p.run()
+	schedule.enter(inc, 0, run_proxy, (p, inc))  
 
 if __name__ == '__main__':
-	loop = asyncio.get_event_loop()
+	p = ProxyService()
+
+	p.run()
+	# schedule.enter(0, 0, run_proxy, (p, 120))
+	# schedule.run()
